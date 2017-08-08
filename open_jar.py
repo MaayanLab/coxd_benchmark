1d0eaecf726788c367d44d8ec09844e56111717a
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-08-08 19:17:01 -0400

    Grid

--- open_jar.py
+++ open_jar.py
@@ -3,7 +3,7 @@ __author__ = 'maximkuleshov'
 import dill as pickle
 import matplotlib.pyplot as plt
 from matplotlib import rc, rcParams, rcParamsDefault
-from math import log10
+from math import log10, sqrt
 
 
 def res2plot(result):
@@ -20,47 +20,53 @@ def res2plot(result):
     return [rank_levels, indices, length]
 
 
-def plot_ranks(levels, length):
-    plt.axes([0.15, 0.68, 0.8, 0.27])
-    plt.xlim([0, length])
-    plt.plot(levels)
-    plt.ylabel('rank level, %', fontsize=10, labelpad=15)
+def plot_ranks(levels, length, ax):
+    ax.set_xlim([0, length])
+    ax.plot(levels)
+    # ax.set_ylabel('rank level, %')
 
 
-def plot_corr(indices, length):
+def plot_corr(indices, length, ax):
     x = sorted(indices.keys())
     y = [indices[key][0] for key in x]
 
-    plt.axes([0.15, 0.37, 0.8, 0.27])
-    plt.xlim([0, length])
-    plt.scatter(x, y)
-    plt.ylabel('-log10(p-value)*corr', fontsize=10, labelpad=15)
+    ax.set_xlim([0, length])
+    ax.scatter(x, y)
+    # ax.set_ylabel('-log10(p-value)*corr')
 
 
-def plot_dist(indices, length):
+def plot_dist(indices, length, ax):
     x = sorted(indices.keys())
     y = [indices[key][1] for key in x]
 
-    plt.axes([0.15, 0.06, 0.8, 0.27])
-    plt.xlim([0, length])
-    plt.scatter(x, y)
-    plt.ylabel('-log10(p-value)*(3-dist)', fontsize=10, labelpad=15)
+    ax.set_xlim([0, length])
+    ax.scatter(x, y)
+    # ax.set_ylabel('-log10(p-value)*(3-dist)')
+    ax.set_xlabel('position')
 
 
 def main():
     chea = 'ChEA_2016'
     direction = 'up'
-    myc0 = pickle.load(open('%s_%s_pval.05.pickle' % (chea, direction), 'rb'))[1]['MYC']
+    myc = pickle.load(open('%s_%s_pval.05.pickle' % (chea, direction), 'rb'))[1]['MYC']
 
-    plot_data = res2plot(myc0['gene:1007'])
+    fig_width = 12
+    fig_height = 8
+    rows = int(sqrt(len(myc.keys())))
+    cols = rows+1
 
-    plt.figure(figsize=(6, 8))
-    plot_ranks(plot_data[0], plot_data[2])
-    plot_corr(plot_data[1], plot_data[2])
-    plot_dist(plot_data[1], plot_data[2])
+    fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(fig_width, fig_height))
 
-    plt.xlabel('position')
+    for i, key in enumerate(sorted(myc.keys())):
+        plot_data = res2plot(myc[key])
+        plot_ranks(plot_data[0], plot_data[2], axes.flat[i])
+        plot_corr(plot_data[1], plot_data[2], axes.flat[i])
+        plot_dist(plot_data[1], plot_data[2], axes.flat[i])
+
+    plt.tight_layout()
     plt.show()
+
+
 
     return
 
